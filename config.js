656e2b0bd18fcb5c946d202c21bc463d7a5a050b
babybebe974-arch <babybebe974@gmail.com>
2026-05-30 17:19:03 +0400

    Add configuration file for API and Sheets

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js - Configuration finale
+window.CONFIG = {
+  AI_API_KEY: 'sk-ant-api03-ICI_TA_VRAIE_CLE_API',
+  SHEETS: {
+    W: 'https://script.google.com/macros/s/AKfycbw_18QVZbRY7M8cNpnx7H4TkoTGMf-JvIAkzS1ScOfrDtHU/exec',
+    N: 'https://script.google.com/macros/s/AKfycbw_17IvW07JgkvvBhrxSRo0XaI9xapmF1ZhxAF5tQZmXrCI/exec'
+  }
+};
